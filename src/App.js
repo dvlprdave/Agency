bd83fa3a8fead50ae5600a7c4c8b1184884b5924
David <devdavee@gmail.com>
2020-03-27 19:29:50 -0400

    added gsap and overlay animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useEffect }from "react";
+import gsap from 'gsap';
 import "./styles/App.scss";
+
 import Header from './components/header';
 import Banner from "./components/banner";
 import Cases from "./components/cases";
@@ -10,6 +12,27 @@ function App() {
   useEffect(() => {
     let vh = window.innerHeight * .01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+    // Prevents text flash on load
+    gsap.to('body', 0, {
+      css: { visibility: 'visible' }
+    } )
+
+    // GSAP Timeline
+    const tl = gsap.timeline()
+    tl.from('.line span', 1.8, {
+      y: 100,
+      ease: 'power4.out',
+      delay: 1,
+      sewY: 7,
+      stagger: { amount: 0.3 }
+    }).to('.overlay-top', 1.6, {
+      height: 0,
+      ease: 'expo.inOut',
+      stagger: 0.4
+    })
+    
+
   }, [])
 
   return (
